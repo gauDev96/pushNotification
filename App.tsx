e2c426de7ae02716faa298a879d2e7a96a6036c9
Gaurab-web <gaurab.officialyt@gmail.com>
2023-11-07 02:54:47 +0530

    gm

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React, { FC, useEffect } from "react";
+import PushNotifiee from "./src/PushNotifiee";
+
+const App:FC = ()=>{
+  return(
+    <PushNotifiee />
+  )
+}
+
+export default App;
---
+++ src/PushNotifiee.tsx
@@ -0,0 +1,49 @@
+import React, { FC, useEffect } from "react";
+import { View, Text } from "react-native";
+import { PermissionsAndroid } from 'react-native';
+import messaging, { FirebaseMessagingTypes } from '@react-native-firebase/messaging';
+import PushNotification from "react-native-push-notification";
+
+
+const PushNotifiee: FC = () => {
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+
+    useEffect(() => {
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+            showNotification(remoteMessage.notification!);
+
+        });
+
+        // To generate FCM Token
+        const checkToken = async (): Promise<void> => {
+            const fcmToken = await messaging().getToken();
+            if (fcmToken) {
+                console.log(fcmToken);
+            }
+        }
+        checkToken()
+
+        return unsubscribe;
+    }, []);
+
+    // Access android local notification
+
+    const showNotification = (notification: FirebaseMessagingTypes.Notification) => {
+        PushNotification.localNotification({ title: notification.title, message: String(notification.body) });
+    }
+    return (
+        <View style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <Text style={{
+                fontSize: 22,
+                fontWeight: '600'
+            }}>Push Notification</Text>
+        </View>
+    )
+}
+
+export default PushNotifiee;
